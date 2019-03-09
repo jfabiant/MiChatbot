506366fa9e1ee92b1d1dbdd57296d37d410281f9
jfabiantorres <john.timoteo@tecsup.edu.pe>
2019-03-09 12:37:20 -0500

    Chatbot demo

---
+++ app/src/main/java/pe/edu/tecsup/jfabiant/michatbot/ClickListener.java
@@ -0,0 +1,13 @@
+package pe.edu.tecsup.jfabiant.michatbot;
+
+import android.view.View;
+
+/**
+ * Created by VMac on 06/01/17.
+ */
+
+public interface ClickListener {
+    void onClick(View view, int position);
+
+    void onLongClick(View view, int position);
+}
